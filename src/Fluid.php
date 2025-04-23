7fd9106ff996923c2a9f1dd33ade6f18b9ec20c2
CapitanPNG <pierniccologiannelli@gmail.com>
2025-04-23 19:13:26 +0000

    fluid

---
+++ src/Fluid.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+namespace Solenoid\X;
+
+
+
+interface Fluid
+{
+    public function find ();
+    public function list ();
+    public function update ();
+    public function insert ();
+    public function delete ();
+}
+
+
+
+?>
