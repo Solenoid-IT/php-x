d0fd74f54dcbb160bacb6e588576dc1b7d2785c4
CapitanPNG <pierniccologiannelli@gmail.com>
2025-09-03 05:55:36 +0000

    error map

--- src/Error.php
+++ src/Error.php
@@ -8,18 +8,36 @@ namespace Solenoid\X;
 
 class Error
 {
+    private static array $map = [];
+
+
+
     public readonly int    $code;
-    public readonly string $message;
+    public readonly string $name;
     public readonly string $description;
 
 
 
-    public function __construct (int $code, string $message = '', string $description = '')
+    public function __construct (int $code, string $name = '', string $description = '')
     {
         // (Getting the values)
         $this->code        = $code;
-        $this->message     = $message;
+        $this->name        = $name;
         $this->description = $description;
+    }
+
+
+
+    public static function get (int $code) : self|null
+    {
+        // Returning the value
+        return self::$map[ $code ] ?? null;
+    }
+
+    public static function register (self $error) : void
+    {
+        // (Getting the value)
+        self::$map[ $error->code ] = $error;
     }
 
 
@@ -27,7 +45,7 @@ class Error
     public function __toString () : string
     {
         // Returning the value
-        return "Error {$this->code} :: {$this->message} :: {$this->description}";
+        return "Error {$this->code} :: {$this->name} :: {$this->description}";
     }
 }
 
