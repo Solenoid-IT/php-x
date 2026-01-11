7d13f54088a56bd276b9829d5d257c944a2a9c71
CapitanPNG <pierniccologiannelli@gmail.com>
2026-01-11 13:44:51 +0000

    Input IntValue not required bugfix

--- src/Input/IntValue.php
+++ src/Input/IntValue.php
@@ -28,7 +28,7 @@ class IntValue extends Value
 
 
         if ( $this->required )
-        {// Value not found
+        {// (Value is required)
             if ( $value === null || $value === '' )
             {// Value not found
                 // (Getting the value)
@@ -37,39 +37,47 @@ class IntValue extends Value
                 // Returning the value
                 return false;
             }
-        }
 
 
 
-        if ( !filter_var( $value, FILTER_VALIDATE_INT ) )
-        {// (Validation failed)
+            if ( !filter_var( $value, FILTER_VALIDATE_INT ) )
+            {// (Validation failed)
+                // (Getting the value)
+                $this->error = "$error_prefix Must be an integer";
+
+                // Returning the value
+                return false;
+            }
+
+
+
             // (Getting the value)
-            $this->error = "$error_prefix Must be an integer";
+            $this->value = (int) $value;
 
-            // Returning the value
-            return false;
+            if ( $this->min !== null && $this->value < $this->min )
+            {// (Validation failed)
+                // (Getting the value)
+                $this->error = "$error_prefix Must be a number >= " . $this->min . ( $this->max === null ? '' : ' and <= ' . $this->max );
+
+                // Returning the value
+                return false;
+            }
+
+            if ( $this->max !== null && $this->value > $this->max )
+            {// (Validation failed)
+                // (Getting the value)
+                $this->error = "$error_prefix Must be a number " . ( $this->min === null ? '' : '>= ' . $this->min . ' and ' ) . '<= ' . $this->max;
+                // Returning the value
+                return false;
+            }
         }
-
-
-
-        // (Getting the value)
-        $this->value = (int) $value;
-
-        if ( $this->min !== null && $this->value < $this->min )
-        {// (Validation failed)
-            // (Getting the value)
-            $this->error = "$error_prefix Must be a number >= " . $this->min . ( $this->max === null ? '' : ' and <= ' . $this->max );
-
-            // Returning the value
-            return false;
-        }
-
-        if ( $this->max !== null && $this->value > $this->max )
-        {// (Validation failed)
-            // (Getting the value)
-            $this->error = "$error_prefix Must be a number " . ( $this->min === null ? '' : '>= ' . $this->min . ' and ' ) . '<= ' . $this->max;
-            // Returning the value
-            return false;
+        else
+        {// (Value is not required)
+            if ( $value === null || $value === '' )
+            {// Value not found
+                // (Setting the value)
+                $this->value = null;
+            }
         }
 
 
@@ -78,7 +86,7 @@ class IntValue extends Value
         return true;
     }
 
-    public function get_value () : int
+    public function get_value () : int|null
     {
         // Returning the value
         return $this->value;
