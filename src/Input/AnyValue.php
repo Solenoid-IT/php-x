f9e8b11daf0e4ec9dd37441073f6cb07266d85b0
CapitanPNG <pierniccologiannelli@gmail.com>
2026-01-20 13:50:03 +0000

    model where_tuple support

--- src/Input/AnyValue.php
+++ src/Input/AnyValue.php
@@ -12,7 +12,7 @@ class AnyValue extends Value
 
 
 
-    public function __construct (string $name, bool $required = true, string $description = '')
+    public function __construct (string $name, bool $required = true, string $description = '', public array $types = [])
     {
         // (Calling the function)
         parent::__construct( $name, $required, $description );
@@ -45,6 +45,94 @@ class AnyValue extends Value
         }
         else
         {// (Value is set)
+            // (Setting the value)
+            $type_match = false;
+
+            foreach ( $this->types as $type )
+            {// Processing each entry
+                switch ( $type )
+                {
+                    case 'int':
+                        if ( is_int( $value ) )
+                        {// Match failed
+                            // (Setting the value)
+                            $type_match = true;
+
+                            // Breaking the iteration
+                            break 2;
+                        }
+                    break;
+
+                    case 'float':
+                        if ( is_float( $value ) )
+                        {// Match failed
+                            // (Setting the value)
+                            $type_match = true;
+
+                            // Breaking the iteration
+                            break 2;
+                        }
+                    break;
+
+                    case 'string':
+                        if ( is_string( $value ) )
+                        {// Match failed
+                            // (Setting the value)
+                            $type_match = true;
+
+                            // Breaking the iteration
+                            break 2;
+                        }
+                    break;
+
+                    case 'bool':
+                        if ( is_bool( $value ) )
+                        {// Match failed
+                            // (Setting the value)
+                            $type_match = true;
+
+                            // Breaking the iteration
+                            break 2;
+                        }
+                    break;
+
+                    case 'array':
+                        if ( is_array( $value ) )
+                        {// Match failed
+                            // (Setting the value)
+                            $type_match = true;
+
+                            // Breaking the iteration
+                            break 2;
+                        }
+                    break;
+
+                    default:
+                        if ( $value instanceof $type )
+                        {// Match failed
+                            // (Setting the value)
+                            $type_match = true;
+
+                            // Breaking the iteration
+                            break 2;
+                        }
+                    break;
+                }
+            }
+
+
+
+            if ( !$type_match )
+            {// (Type does not match)
+                // (Getting the value)
+                $this->error = "$error_prefix Value type does not match the expected types";
+
+                // Returning the value
+                return false;
+            }
+
+
+
             // (Getting the value)
             $this->value = $value;
         }
