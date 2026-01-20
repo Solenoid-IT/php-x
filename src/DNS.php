0f1a94d6d238ce34b3746ae3a9452558d808d59a
CapitanPNG <pierniccologiannelli@gmail.com>
2026-01-20 13:07:13 +0000

    DNS query

--- src/DNS.php
+++ src/DNS.php
@@ -24,6 +24,49 @@ class DNS
         // Returning the value
         return $ip[0]['ip'];
     }
+
+    public static function query (string $fqdn, ?string $server = null) : array|false
+    {
+        // (Setting the value)
+        $records = [];
+
+
+
+        // (Getting the value)
+        $result = shell_exec( 'dig' . ( $server ? ' @' . escapeshellarg( $server ) : '' ) . ' ' . escapeshellarg( $fqdn ) . ' +noall +answer' );
+
+        if ( !$result )
+        {// (There are no records)
+            // Returning the value
+            return false;
+        }
+
+
+
+        foreach ( explode( "\n", $result ) as $record )
+        {// Processing each entry
+            // (Getting the value)
+            $parts = preg_split( '/\s+/', trim( $record ) );
+
+
+
+            // (Appending the value)
+            $records[] =
+            [
+                'name'  => $parts[0],
+                'ttl'   => (int) $parts[1],
+                'class' => $parts[2],
+                'type'  => $parts[3],
+                'value' => implode( ' ', array_slice( $parts, 4 ) )
+            ]
+            ;
+        }
+
+
+
+        // Returning the value
+        return $records;
+    }
 }
 
 
