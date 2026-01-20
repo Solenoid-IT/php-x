d16a8585e29b548fde9c04c99bd2d95701dfa0a9
CapitanPNG <pierniccologiannelli@gmail.com>
2026-01-20 00:04:38 +0000

    input EnumValue

---
+++ src/Input/EnumValue.php
@@ -0,0 +1,89 @@
+<?php
+
+
+
+namespace Solenoid\X\Input;
+
+
+
+class EnumValue extends Value
+{
+    const TYPE = 'array';
+
+
+
+    public function __construct (string $name, bool $required = true, string $description = '', public array $values = [])
+    {
+        // (Calling the function)
+        parent::__construct( $name, $required, $description );
+    }
+
+
+
+    public function validate (mixed $value) : bool
+    {
+        // (Getting the value)
+        $error_prefix = self::TYPE . ' ' . $this->name . ' ' . '::';
+
+
+
+        if ( $value === null || $value === '' )
+        {// (Value is not set)
+            // (Setting the value)
+            $this->value = null;
+
+
+
+            if ( $this->required )
+            {// (Value is required)
+                // (Getting the value)
+                $this->error = "$error_prefix Value is required";
+
+                // Returning the value
+                return false;
+            }
+        }
+        else
+        {// (Value is set)
+            if ( !is_array( $value ) )
+            {// Match failed
+                // (Getting the value)
+                $this->error = "$error_prefix Value must be an array";
+
+                // Returning the value
+                return false;
+            }
+
+
+
+            if ( !in_array( $value, $this->values ) )
+            {// Match failed
+                // (Getting the value)
+                $this->error = "$error_prefix Value is not in the allowed set";
+
+                // Returning the value
+                return false;
+            }
+
+
+
+            // (Getting the value)
+            $this->value = $value;
+        }
+
+
+
+        // Returning the value
+        return true;
+    }
+
+    public function get_value () : array
+    {
+        // Returning the value
+        return $this->value;
+    }
+}
+
+
+
+?>
