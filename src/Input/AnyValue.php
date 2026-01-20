2b10981e6b26ac9355ca436b141834c7f91b3c16
CapitanPNG <pierniccologiannelli@gmail.com>
2026-01-20 13:54:15 +0000

    input AnyValue type match

--- src/Input/AnyValue.php
+++ src/Input/AnyValue.php
@@ -45,90 +45,93 @@ class AnyValue extends Value
         }
         else
         {// (Value is set)
-            // (Setting the value)
-            $type_match = false;
+            if ( !empty( $this->types ) )
+            {// Value is not empty
+                // (Setting the value)
+                $type_match = false;
 
-            foreach ( $this->types as $type )
-            {// Processing each entry
-                switch ( $type )
-                {
-                    case 'bool':
-                        if ( is_bool( $value ) )
-                        {// Match failed
-                            // (Setting the value)
-                            $type_match = true;
+                foreach ( $this->types as $type )
+                {// Processing each entry
+                    switch ( $type )
+                    {
+                        case 'bool':
+                            if ( is_bool( $value ) )
+                            {// Match failed
+                                // (Setting the value)
+                                $type_match = true;
 
-                            // Breaking the iteration
-                            break 2;
-                        }
-                    break;
+                                // Breaking the iteration
+                                break 2;
+                            }
+                        break;
 
-                    case 'int':
-                        if ( is_int( $value ) )
-                        {// Match failed
-                            // (Setting the value)
-                            $type_match = true;
+                        case 'int':
+                            if ( is_int( $value ) )
+                            {// Match failed
+                                // (Setting the value)
+                                $type_match = true;
 
-                            // Breaking the iteration
-                            break 2;
-                        }
-                    break;
+                                // Breaking the iteration
+                                break 2;
+                            }
+                        break;
 
-                    case 'float':
-                        if ( is_float( $value ) )
-                        {// Match failed
-                            // (Setting the value)
-                            $type_match = true;
+                        case 'float':
+                            if ( is_float( $value ) )
+                            {// Match failed
+                                // (Setting the value)
+                                $type_match = true;
 
-                            // Breaking the iteration
-                            break 2;
-                        }
-                    break;
+                                // Breaking the iteration
+                                break 2;
+                            }
+                        break;
 
-                    case 'string':
-                        if ( is_string( $value ) )
-                        {// Match failed
-                            // (Setting the value)
-                            $type_match = true;
+                        case 'string':
+                            if ( is_string( $value ) )
+                            {// Match failed
+                                // (Setting the value)
+                                $type_match = true;
 
-                            // Breaking the iteration
-                            break 2;
-                        }
-                    break;
+                                // Breaking the iteration
+                                break 2;
+                            }
+                        break;
 
-                    case 'array':
-                        if ( is_array( $value ) )
-                        {// Match failed
-                            // (Setting the value)
-                            $type_match = true;
+                        case 'array':
+                            if ( is_array( $value ) )
+                            {// Match failed
+                                // (Setting the value)
+                                $type_match = true;
 
-                            // Breaking the iteration
-                            break 2;
-                        }
-                    break;
+                                // Breaking the iteration
+                                break 2;
+                            }
+                        break;
 
-                    default:
-                        if ( $value instanceof $type )
-                        {// Match failed
-                            // (Setting the value)
-                            $type_match = true;
+                        default:
+                            if ( $value instanceof $type )
+                            {// Match failed
+                                // (Setting the value)
+                                $type_match = true;
 
-                            // Breaking the iteration
-                            break 2;
-                        }
-                    break;
+                                // Breaking the iteration
+                                break 2;
+                            }
+                        break;
+                    }
                 }
-            }
 
 
 
-            if ( !$type_match )
-            {// (Type does not match)
-                // (Getting the value)
-                $this->error = "$error_prefix Value type does not match the expected types";
+                if ( !$type_match )
+                {// (Type does not match)
+                    // (Getting the value)
+                    $this->error = "$error_prefix Value type does not match the expected types";
 
-                // Returning the value
-                return false;
+                    // Returning the value
+                    return false;
+                }
             }
 
 
