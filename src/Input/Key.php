ccd901e09fcde790b60b0b4e2e7d6907b43e1f02
CapitanPNG <pierniccologiannelli@gmail.com>
2026-01-21 13:44:07 +0000

    input DTO get_keys

--- src/Input/Key.php
+++ src/Input/Key.php
@@ -10,7 +10,7 @@ use \Attribute;
 
 
 
-#[ Attribute( Attribute::TARGET_METHOD | Attribute::IS_REPEATABLE ) ]
+#[ Attribute( Attribute::TARGET_CLASS | Attribute::IS_REPEATABLE ) ]
 class Key
 {
     public function __construct (public string $name, public array $fields) {}
