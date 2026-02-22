baa485a1198c133bfe401ef5db0266e8559aeae8
CapitanPNG <pierniccologiannelli@gmail.com>
2026-02-22 16:13:00 +0100

    App run method

--- src/App.php
+++ src/App.php
@@ -7,6 +7,7 @@ namespace Solenoid\X;
 
 
 use \Solenoid\X\Error;
+use \Solenoid\X\Input\Validator;
 
 
 
@@ -151,6 +152,128 @@ class App
 
         // Returning the value
         return $this;
+    }
+
+
+
+    public function run (string $class, string $method) : mixed
+    {
+        // (Setting the value)
+        $middlewares = [];
+
+        foreach ( ( new \ReflectionMethod( $class, $method ) )->getAttributes( Middleware::class ) as $attribute )
+        {// Processing each entry
+            // (Getting the value)
+            $middlewares = ( $attribute->newInstance() )->pipes;
+
+            // Breaking the iteration
+            break;
+        }
+
+
+
+        // (Getting the value)
+        $input_flow = function () use ($class, $method)
+        {
+            // (Getting the value)
+            $instance = $this->container->make_instance( $class );
+
+
+
+            // (Setting the value)
+            $params = [];
+
+
+
+            // (Getting the value)
+            $validator = new Validator( $class, $method );
+
+            if ( $validator->available() )
+            {// Value is true
+                // (Getting the value)
+                $request = $this->container->make( 'request' );
+
+
+
+                // (Getting the value)
+                $input_type = $validator->get_input_type();
+
+
+
+                // (Getting the value)
+                $input = in_array( $input_type, [ 'DTO', 'ArrayList' ] ) ? $request->json( true ) : $request->buffer();
+
+
+
+                // (Getting the value)
+                $error = $validator->check( $input );
+
+                if ( $error !== null )
+                {// (Check failed)
+                    // (Getting the value)
+                    $response = $this->container->make( 'response' );
+
+
+
+                    if ( $error instanceof \stdClass || is_array( $error ) )
+                    {// (Error of a DTO or ArrayList)
+                        // Returning the value
+                        return $response->json( 400, $error );
+                    }
+                    else
+                    {// (Error of a Value)
+                        // Returning the value
+                        return $response->text( 400, $error );
+                    }
+                }
+                else
+                {// (Check passed)
+                    switch ( $input_type )
+                    {// Processing each type
+                        case 'Value':
+                            // (Getting the value)
+                            $params = [ $validator->get_value() ];
+                        break;
+
+                        case 'DTO':
+                            // (Getting the value)
+                            $dto = $validator->get_value();
+
+
+
+                            // (Getting the value)
+                            $params =
+                            [
+                                get_class( $dto ) => $dto,
+                                'dto'             => $dto
+                            ]
+                            ;
+
+                            // (Getting the value)
+                            $params = array_merge( $params, (array) $dto );
+                        break;
+
+                        case 'ArrayList':
+                            // (Getting the value)
+                            $params = [ $validator->get_value() ];
+
+                            # ahcid List<DTO> to implementt
+                        break;
+                    }
+                }
+            }
+
+
+
+            // Returning the value
+            return $this->container->run_instance_method( $instance, $method, $params );
+        }
+        ;
+
+
+
+        // Returning the value
+        return ( new Dispatcher( $this->container ) )->dispatch( $this->container->make( 'request' ), $middlewares, $input_flow );
     }
 }
 
