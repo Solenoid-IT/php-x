ae199c6adc97bb2d90358bcd51369cc4294582fd
CapitanPNG <pierniccologiannelli@gmail.com>
2026-03-04 20:02:34 +0100

    Logger push

--- src/Logger.php
+++ src/Logger.php
@@ -69,7 +69,7 @@ class Logger
 
 
 
-    public function push (int|string $level, string $message) : self
+    public function push (string $message, int|string $level = 'info') : self
     {
         // (Getting the value)
         $level = is_string( $level ) ? self::MAP[ $level ] : $level;
